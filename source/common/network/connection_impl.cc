7fbfcb83b6b6acb8f881406076d251669c11e784
Ubuntu <ubuntu@ip-172-31-8-41.ap-south-1.compute.internal>
2021-02-04 04:06:21 +0000

    debug

--- source/common/network/connection_impl.cc
+++ source/common/network/connection_impl.cc
@@ -613,7 +613,7 @@ ConnectionImpl::unixSocketPeerCredentials() const {
 }
 
 void ConnectionImpl::onWriteReady() {
-  ENVOY_CONN_LOG(trace, "anjana write ready", *this);
+  ENVOY_CONN_LOG(info, "anjana write ready", *this);
 
   if (connecting_) {
     int error;
